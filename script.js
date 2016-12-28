b6a025de5ea42630d74c3f75775d32e75e5cfc9b
Your Name <youremail@domain.com>
2016-12-28 23:40:11 +0100

    8

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+function text1(){
+
+    document.getElementById("center").style.display = "block";
+    document.getElementById("center2").style.display = "none";
+
+}
+
+function text2(){
+
+      document.getElementById("center2").style.display = "block";
+      document.getElementById("center").style.display = "none";
+
+
+}
